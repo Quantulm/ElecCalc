00e6fd5600a98dfd31f82305215d730e7d8b79b0
AlexHls <alex.holas@gmx.de>
2021-07-11 00:17:38 +0200

    Added emcee sampling including correlated data

--- lectureformat/lecture.py
+++ lectureformat/lecture.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as ps
+import pandas as pd
+import emcee
+import corner
+
 
 class lecture():
     """Lecture class. Creates an object for one single lecture."""
-
 
     def __init__(self, dataframe):
         """
@@ -62,8 +64,28 @@ class lecture():
             self.beamer = dataframe["Beamer"]
         except KeyError:
             self.beamer = None
+        try:
+            self.num_lec = dataframe["NumLec"]
+        except KeyError:
+            self.num_lec = None
+
+        self.onsite_sampler = None
+        self.online_sampler = None
 
         return
+
+    def get_onsite(self):
+        """Function returning all data classified as 'onsite' data"""
+        return np.array(
+            [
+                self.electronic_onsite,
+                self.air_conditioning,
+                self.transportation,
+                self.lighting_onsite,
+                self.beamer,
+                self.num_lec,
+            ]
+        )
 
     def check_onsite(self):
         """Check if all variables for the calculation of the onsite model are present"""
@@ -75,9 +97,10 @@ class lecture():
             return False
         elif self.beamer is None:
             return False
+        elif self.num_lec is None:
+            return False
         else:
             return True
-
 
     def check_online(self):
         """Check if all variables for the calculation of the online model are present"""
@@ -87,24 +110,62 @@ class lecture():
             return False
         elif self.lighting_online is None:
             return False
+        elif self.num_lec is None:
+            return False
         else:
             return True
 
-
     def caluclate_onsite(self):
         """Function to calculate the onsite model"""
-        pass
 
+        # Step 1: Check if all data is present
+        if not self.check_onsite():
+            raise ValueError("Not enough data present for the calculation of the onsite result.")
+
+        # Step 2: Create covariance matrix and mean vector
+        data = self.get_onsite()
+
+        cov = np.cov(data)
+
+        mean = np.mean(data, axis=1)
+
+        # Step 3: Define multivariate Gaussian density
+        def log_prob(x, mean, cov):
+            diff = x - mean
+            return -0.5 * np.dot(diff, np.linalg.solve(cov, diff))
+
+        # Step 4: Set up ensemble sampler
+        nwalkers = 32
+        ndim = len(mean)
+
+        p0 = np.random.rand(nwalkers, ndim) # Initial value
+
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, args=[mean, cov])
+
+        # Step 5: Run a short burn in
+        state = sampler.run_mcmc(p0, 100)
+        sampler.reset()
+
+        # Step 6: Run production
+        sampler.run_mcmc(state, 5000)
+
+        self.onsite_sampler = sampler
+
+        return
 
     def plot_onsite(self):
         """Function to create the plot of the onsite model"""
-        pass
+        flat_samples = self.onsite_sampler.get_chain(discard=100, thin=15, flat=True)
 
+        fig = corner.corner(
+            flat_samples
+        )
+
+        return fig
 
     def calculate_online(self):
         """Function to calculate online model"""
         pass
-
 
     def plot_online(self):
         """Function to create the plot of the online model"""
